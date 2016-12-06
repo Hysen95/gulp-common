98346d6534a00ee4b4b459ae63871a7b372d0593
Gabriele Cipolloni <gcipolloni@key-biz.it>
2016-12-06 17:11:49 +0100

    Refactoring folders.

--- gulpfile.js
+++ gulpfile.js
@@ -2,111 +2,113 @@
 
 /* VARS */
 var gulp = require('gulp');
-var minify = require('gulp-minify');
+var minifyJS = require('gulp-minify');
 var sequence = require('gulp-sequence');
 var clean = require('gulp-clean');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
-// var jslint = require('gulp-jslint');
+var minifyCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
 /* VARS */
 
 /* CONSTS */
-const CSS_DIR = "./css";
-const JS_DIR = "./js";
-const SASS_DIR = "./sass";
+const SRC_DIR = "./src";
+const JS_SRC_DIR = SRC_DIR + "/js";
+const SASS_SRC_DIR = SRC_DIR + "/sass";
+
+const DIST_DIR = "./dist";
+const CSS_DIST_DIR = DIST_DIR + "/css";
+const JS_DIST_DIR = DIST_DIR + "/js";
 /* CONSTS */
- 
-/* TASKS */
 
-	/* CSS */
-	gulp.task('css-clean', function() {
-	  return gulp.src([CSS_DIR + "/*.css", "!" + CSS_DIR + "/*.min.css"], {read: false})
-		.pipe(clean());
-	});
-	gulp.task('css-min-clean', function() {
-	  return gulp.src(CSS_DIR + "/*.min.css", {read: false})
-		.pipe(clean());
-	});
-	
-	gulp.task('css-all-in-one', function() {
-	  return gulp.src([CSS_DIR + "/*.min.css", "!" + CSS_DIR + "/all.min.css"])
-		.pipe(concat('all.min.css'))
-		.pipe(gulp.dest(CSS_DIR));
-	});
+/* CSS */
+gulp.task('css-clean', function() {
+  return gulp.src(CSS_DIST_DIR, {read: false})
+	.pipe(clean());
+});
 
+gulp.task('css-minify', function() {
+  return gulp.src([CSS_DIST_DIR + "/*.css", "!" + CSS_DIST_DIR + "/*.min.css"])
+	.pipe(minifyCSS({
+		compatibility: 'ie8'
+	}))
+	.pipe(rename({
+		suffix: ".min"
+	}))
+	.pipe(gulp.dest(CSS_DIST_DIR));
+});
 	
-		/* SASS */
-		gulp.task('sass-compile', function () {
-		  return gulp.src(SASS_DIR + "/*.scss")
-			.pipe(sass().on('error', sass.logError))
-			.pipe(gulp.dest(CSS_DIR));
-		});
-	
-		gulp.task('sass-watch', function() {
-			gulp.watch(SASS_DIR + "/*.scss", ["css-build"]);
-		});
-		/* SASS */
+gulp.task('css-all-in-one', function() {
+  return gulp.src([CSS_DIST_DIR + "/*.min.css", "!" + CSS_DIST_DIR + "/all.min.css"])
+	.pipe(concat('all.min.css'))
+	.pipe(gulp.dest(CSS_DIST_DIR));
+});
 
-	gulp.task("css-build", function(callback) {
-		sequence(["sass-compile", "css-all-in-one"])(callback);
-	});
-	gulp.task("css-clean-build", sequence(["css-clean", "css-min-clean", "css-build"]));
-	/* CSS */
+gulp.task("css-build", function(callback) {
+	sequence("sass-compile", "css-minify", "css-all-in-one")(callback);
+});
+gulp.task("css-clean-build", sequence("css-clean", "css-build"));
+/* CSS */
 
-	/* JS */
-	gulp.task('js-clean', function() {
-	  return gulp.src(JS_DIR + "/*.min.js", {read: false})
-		.pipe(clean());
-	});
-	
-	gulp.task('js-minify', function() {
-	  return gulp.src([JS_DIR + "/*.js", "!" + JS_DIR + "/*.min.js"])
-		.pipe(minify({
-			ext: {
-				min: ".min.js"
-			}
-		}))
-		.pipe(gulp.dest(JS_DIR))
-	});
-	
-	gulp.task('js-all-in-one', function() {
-	  return gulp.src([JS_DIR + "/*.min.js", "!" + JS_DIR + "/all.min.js"])
-		.pipe(concat('all.min.js'))
-		.pipe(gulp.dest(JS_DIR));
-	});
-	
-	// gulp.task("js-lint", function() {
-		
-	// });
-	
-	gulp.task('js-watch', function() {
-		gulp.watch(JS_DIR + "/*.js", ["js-build"]);
-	});
+/* SASS */
+gulp.task('sass-compile', function () {
+  return gulp.src(SASS_SRC_DIR + "/*.scss")
+	.pipe(sass().on('error', sass.logError))
+	.pipe(gulp.dest(CSS_DIST_DIR));
+});
 
-	gulp.task("js-build", function(callback) {
-		sequence(["js-minify", "js-all-in-one"])(callback);
-	});
-	gulp.task("js-clean-build", sequence(["js-clean", "js-build"]));
-	/* JS */
+gulp.task('sass-watch', function() {
+	gulp.watch(SASS_SRC_DIR + "/*.scss", ["css-build"]);
+});
+/* SASS */
 
-	/* COMMONS */
-	
-	gulp.task('watch', function() {
-		gulp.watch([
-			SASS_DIR + "/*.scss", 
-			JS_DIR + "/*.js"
-		], ["clean-build"]);
-	});
-	
-	gulp.task("clean", ["css-clean", "js-clean"]);
+/* JS */
+gulp.task('js-clean', function() {
+  return gulp.src(JS_DIST_DIR, {read: false})
+	.pipe(clean());
+});
 
-	gulp.task("build", ["css-build", "js-build"]);
+gulp.task('js-minify', function() {
+  return gulp.src([JS_SRC_DIR + "/*.js", "!" + JS_SRC_DIR + "/*.min.js"])
+	.pipe(minifyJS({
+		ext: {
+			min: ".min.js"
+		}
+	}))
+	.pipe(gulp.dest(JS_DIST_DIR))
+});
 
-	gulp.task("clean-build", function(callback) {
-		sequence(["clean" , "build"])(callback);
-	});
-	
-	gulp.task("default", ["clean-build"]);
-	/* COMMONS */
+gulp.task('js-all-in-one', function() {
+  return gulp.src([JS_DIST_DIR + "/*.min.js", "!" + JS_DIST_DIR + "/all.min.js"])
+	.pipe(concat('all.min.js'))
+	.pipe(gulp.dest(JS_DIST_DIR));
+});
 
-/* TASKS */
+gulp.task('js-watch', function() {
+	gulp.watch(JS_SRC_DIR + "/*.js", ["js-build"]);
+});
+
+gulp.task("js-build", function(callback) {
+	sequence("js-minify", "js-all-in-one")(callback);
+});
+gulp.task("js-clean-build", sequence("js-clean", "js-build"));
+/* JS */
+
+/* COMMONS */
+gulp.task('watch', function() {
+	gulp.watch([
+		SASS_SRC_DIR + "/*.scss", 
+		JS_SRC_DIR + "/*.js"
+	], ["clean-build"]);
+});
+
+gulp.task("clean", ["css-clean", "js-clean"]);
+
+gulp.task("build", ["css-build", "js-build"]);
+
+gulp.task("clean-build", function(callback) {
+	sequence("clean" , "build")(callback);
+});
+
+gulp.task("default", ["clean-build"]);
+/* COMMONS */
