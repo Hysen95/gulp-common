9c248c7b8c4f07699a930ac85926231542349ca4
Gabriele Cipolloni <gcipolloni@key-biz.it>
2016-12-07 10:48:58 +0100

    Update tasks name.

--- gulpfile.js
+++ gulpfile.js
@@ -22,12 +22,12 @@ const JS_DIST_DIR = DIST_DIR + "/js";
 /* CONSTS */
 
 /* CSS */
-gulp.task('css-clean', function() {
+gulp.task('css:clean', function() {
   return gulp.src(CSS_DIST_DIR, {read: false})
 	.pipe(clean());
 });
 
-gulp.task('css-minify', function() {
+gulp.task('css:minify', function() {
   return gulp.src([CSS_DIST_DIR + "/*.css", "!" + CSS_DIST_DIR + "/*.min.css"])
 	.pipe(minifyCSS({
 		compatibility: 'ie8'
@@ -38,37 +38,37 @@ gulp.task('css-minify', function() {
 	.pipe(gulp.dest(CSS_DIST_DIR));
 });
 	
-gulp.task('css-all-in-one', function() {
+gulp.task('css:all-in-one', function() {
   return gulp.src([CSS_DIST_DIR + "/*.min.css", "!" + CSS_DIST_DIR + "/all.min.css"])
 	.pipe(concat('all.min.css'))
 	.pipe(gulp.dest(CSS_DIST_DIR));
 });
 
-gulp.task("css-build", function(callback) {
-	sequence("sass-compile", "css-minify", "css-all-in-one")(callback);
+gulp.task("css:build", function(callback) {
+	sequence("sass:compile", "css:minify", "css:all-in-one")(callback);
 });
-gulp.task("css-clean-build", sequence("css-clean", "css-build"));
+gulp.task("css:clean-build", sequence("css:clean", "css:build"));
 /* CSS */
 
 /* SASS */
-gulp.task('sass-compile', function () {
+gulp.task('sass:compile', function () {
   return gulp.src(SASS_SRC_DIR + "/*.scss")
 	.pipe(sass().on('error', sass.logError))
 	.pipe(gulp.dest(CSS_DIST_DIR));
 });
 
-gulp.task('sass-watch', function() {
-	gulp.watch(SASS_SRC_DIR + "/*.scss", ["css-build"]);
+gulp.task('sass:watch', function() {
+	gulp.watch(SASS_SRC_DIR + "/*.scss", ["css:build"]);
 });
 /* SASS */
 
 /* JS */
-gulp.task('js-clean', function() {
+gulp.task('js:clean', function() {
   return gulp.src(JS_DIST_DIR, {read: false})
 	.pipe(clean());
 });
 
-gulp.task('js-minify', function() {
+gulp.task('js:minify', function() {
   return gulp.src([JS_SRC_DIR + "/*.js", "!" + JS_SRC_DIR + "/*.min.js"])
 	.pipe(minifyJS({
 		ext: {
@@ -78,20 +78,20 @@ gulp.task('js-minify', function() {
 	.pipe(gulp.dest(JS_DIST_DIR))
 });
 
-gulp.task('js-all-in-one', function() {
+gulp.task('js:all-in-one', function() {
   return gulp.src([JS_DIST_DIR + "/*.min.js", "!" + JS_DIST_DIR + "/all.min.js"])
 	.pipe(concat('all.min.js'))
 	.pipe(gulp.dest(JS_DIST_DIR));
 });
 
-gulp.task('js-watch', function() {
+gulp.task('js:watch', function() {
 	gulp.watch(JS_SRC_DIR + "/*.js", ["js-build"]);
 });
 
-gulp.task("js-build", function(callback) {
-	sequence("js-minify", "js-all-in-one")(callback);
+gulp.task("js:build", function(callback) {
+	sequence("js:minify", "js:all-in-one")(callback);
 });
-gulp.task("js-clean-build", sequence("js-clean", "js-build"));
+gulp.task("js:clean-build", sequence("js:clean", "js:build"));
 /* JS */
 
 /* COMMONS */
@@ -102,9 +102,9 @@ gulp.task('watch', function() {
 	], ["clean-build"]);
 });
 
-gulp.task("clean", ["css-clean", "js-clean"]);
+gulp.task("clean", ["css:clean", "js:clean"]);
 
-gulp.task("build", ["css-build", "js-build"]);
+gulp.task("build", ["css:build", "js:build"]);
 
 gulp.task("clean-build", function(callback) {
 	sequence("clean" , "build")(callback);
